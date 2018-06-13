9e6234c721856af9e544492a7d95717f5b178172
Init Built <lauwrentius.m@vmgstudio520.com>
2018-06-13 08:59:22 -0700

    adds htaccess.

--- src/App.js
+++ src/App.js
@@ -25,8 +25,9 @@ class App extends Component {
   }
   initProject = () => {
     const {location,initProject} = this.props
-    const {search} = location
-    const id = search.substring(1)===""?PROJECT1_ID:search.substring(1)
+    const {pathname} = location
+
+    const id = pathname.substring(1)===""?PROJECT1_ID:pathname.substring(1)
     initProject(id)
   }
 
